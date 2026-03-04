cbbc71d6fb372d444823f053ae3723ef1d31564a
Laharika08 <kanchanapallilaharika08@gmail.com>
2026-03-04 12:29:18 -0500

    Add FastAPI application code

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import os
+from datetime import datetime
+
+app = FastAPI()
+
+@app.get("/")
+def health():
+    return {"status": "ok"}
+
+@app.get("/build-info")
+def build_info():
+    return {
+        "commit": os.getenv("COMMIT_SHA", "local"),
+        "build_time": os.getenv("BUILD_TIME", str(datetime.utcnow()))
+    }
